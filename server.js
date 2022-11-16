8eb1bf1a54a02c9a96bd631fb49aa90fac95044d
Elijah Bassett <69734856+BassettUNC@users.noreply.github.com>
2022-11-16 16:17:59 -0500

    changes

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ if (args.port) {
 	port = 5000;
 }
 
+//look
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 // app dir returns 200
 app.get('/app/', (req,res) => {
-	res.status(200).send("200 OK").end();
-})
-
-//default returns 404
-app.get("*", (req, res) => {
-	res.status(404).end()
+    res.status(200),
+    res.send("200 OK")
 })
 
 // default role params
@@ -35,25 +35,49 @@ const sides = 6;
 const dice = 2;
 const rolls = 1;
 
+app.get('/app/roll/', (req, res) => {
+    res.status(200)
+    res.send(roll(sides, dice, rolls))
+})
+
+app.post('/app/roll/', (req, res) => {
+    const sides = parseInt(req.body.sides);
+    const dice = parseInt(req.body.dice);
+    const rolls = parseInt(req.body.rolls);
+    res.status(200);
+    res.send(roll(sides, dice, rolls));    
+})
+
 // sides
 app.post('/app/roll/:sides', (req,res) => {
-	sides = parseInt(req.params.sides)
-	res.status("200").roll(sides,dice,rolls)
+	const sides = parseInt(req.params.sides)
+    res.status(200);
+    res.send(roll(sides, dice, rolls));
 })
 
 // sides, dice
-app.post('/app/roll/:sides/:dice', (req,res) => {
-	sides = parseInt(req.params.sides)
-	dice = parseInt(req.params.dice)
-	res.status("200").roll(sides,dice,rolls)
+app.get('/app/roll/:sides/:dice', (req, res) => {
+    const sides = parseInt(req.params.sides);
+    const dice = parseInt(req.params.dice);
+    res.status(200);
+    res.send(roll(sides, dice, rolls));
 })
 
 // sides, dice, rolls
-app.post('/app/roll/:sides/:dice/:rolls', (req,res) => {
-	sides = parseInt(req.params.sides)
-	dice = parseInt(req.params.dice)
-	rolls = parseInt(req.params.rolls)
-	res.status("200").roll(sides,dice,rolls)
+app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
+    const sides = parseInt(req.params.sides);
+    const dice = parseInt(req.params.dice);
+    const rolls = parseInt(req.params.rolls);
+    res.status(200);
+    res.send(roll(sides, dice, rolls));
 })
 
-app.listen(port);
+//default returns 404
+app.get("*", (req, res) => {
+    res.status(404);
+    res.send("404 NOT FOUND");
+})
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port)
+})
